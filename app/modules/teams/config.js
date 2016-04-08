b24123ee11a4e71da7ac1b6e7b3828870fa90bb1
James McCray <chrisjamesjackmccray@gmail.com>
2016-04-08 11:33:05 -0500

    c

--- app/modules/teams/config.js
+++ app/modules/teams/config.js
@@ -39,28 +39,22 @@ function config($stateProvider) {
     url: '/',
     controller: 'TeamsController as teamCtrl',
     template: require('./views/teams.html')
-  });
-
-  $stateProvider
+  })
   .state('new_team', {
     url: '/new',
     controller: 'NewTeamController as newCtrl',
     template: require('./views/new_team.html')
-  });
-
-  $stateProvider
+  })
   .state('edit_team', {
     url: '/:id/edit',
     controller: 'EditTeamController as editCtrl',
-    template require('./views/edit_team.html')
-  });
-
-  $stateProvider
+    template: require('./views/edit_team.html')
+  })
   .state('team', {
     url: '/:id',
     controller: 'TeamController as teamCtrl',
     template: require('./views/team.html')
-  })
+  });
 
 }
 
